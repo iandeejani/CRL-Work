53435705adbc3f3df984fa90fca2efcdc6bad1fa
Ibraheem Andeejani <67938926+iandeejani@users.noreply.github.com>
2020-08-18 13:54:24 -0400

    Add files via upload

--- 2D-Model.cpp
+++ 2D-Model.cpp
@@ -1,4 +1,3 @@
-// Frickin 5th Project File Lets Make this Work.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // ALRIGHT WE IN THE LAST STRETCH BROSKI LES MAKE PRIYANKA FRIICKIN PROUD SONNNNNNNNNNNN
 
 
@@ -16,6 +15,10 @@
 
 
 using namespace Eigen;
+
+
+
+
 
 class Disk {
 
@@ -43,6 +46,22 @@ public:
 
 };
 
+
+//template?
+template< typename F >
+class gsl_function_pp : public gsl_function {
+public:
+	gsl_function_pp(const F& func) : _func(func) {
+		function = &gsl_function_pp::invoke;
+		params = this;
+	}
+private:
+	const F& _func;
+	static double invoke(double x, void* params) {
+		return static_cast<gsl_function_pp*>(params)->_func(x);
+	}
+};
+
 class Robot {
 
 public:
@@ -55,7 +74,7 @@ public:
 	Matrix<float, 2, 1> f_tend;
 	Robot(float l, float r, float g, int d, float E, Vector2d weights, Matrix<float, 2, 1> f_tend) :d_radius(r), arclength(l), disk_num(d), g(g), E(E), weights(weights), f_tend(f_tend) {
 
-		
+
 	}
 
 	Matrix<float, 3, 3> get_transmat(int q, int p, float l, float theta) {
@@ -82,7 +101,7 @@ public:
 		}
 	}
 
-	Matrix<Vector3f, 2, 1> get_segmentfm(Matrix<Vector3d, 2, 1> p_ten, Matrix<float, 2, 1> f_tend, int i, int n, Matrix<float, 2, 1> weights, Matrix<Vector3d, 2, 1> prev_MF, float l, float th) {
+	Matrix<Vector3f, 2, 1> get_segmentfm(Matrix<Vector3f, 2, 1> p_ten, Matrix<Vector3f, 2, 1> f_tend, int i, int n, Matrix<float, 2, 1> weights, Matrix<Vector3d, 2, 1> prev_MF, float l, float th) {
 		//gets current force and moment on each tendon 
 
 		Matrix<float, 3, 3> T_mat = get_transmat(i, i - 1, l, th); //figure out how to get rid of the need for an l
@@ -91,7 +110,7 @@ public:
 		Vector3f ai = T_mat * (p_ten[0]);
 		Vector3f bi = T_mat * (p_ten[1]); //p_ten houses tendon locations
 
-			//figure out how to combine ai and bi 
+		//figure out how to combine ai and bi 
 
 		ai = (-1 * ai) + p_ten(0);  //ai to ai-1
 		bi = (-1 * bi) + p_ten(1);
@@ -128,21 +147,21 @@ public:
 		Matrix<float, 3, 1> Fg_d = G_mat * weights[0]; //force g on disk i in reference of i-1 MAKE SURE TO DEFINE MASSES[0] AS [0, mass*g, 0]
 		Matrix<float, 3, 1> Mg_d = prev_coord.cross(Fg_d); //moment of Fg_d
 
-		Matrix<float, 3, 1>	MFi = prev_coord.cross(prev_MF[1]);
+		//Vector3f MFi = prev_coord.cross(prev_MF[1][0]);
 
 		Vector3f M1_rel = ai.cross(F1_rel);
 		Vector3f M2_rel = bi.cross(F2_rel);
 
-		prev_MF[1] = T_mat * prev_MF[1]; //puts previous force in terms of i-1 reference frame
-		prev_MF[0] = T_mat * prev_MF[0]; //previous moment in terms of i-1 reference frame
+		Vector3f prevF = T_mat * prev_MF[1][0]; //puts previous force in terms of i-1 reference frame
+		Vector3f prevM = T_mat * prev_MF[0][0]; //previous moment in terms of i-1 reference frame
 
-		Vector3f F_net = F1_rel + F2_rel + prev_MF[1] + Fg_d;
-		Vector3f M_net = M1_rel + M2_rel + prev_MF[0] + Mg_d + MFi;
+		Vector3f F_net = F1_rel + F2_rel + prevF + Fg_d;
+		Vector3f M_net = M1_rel + M2_rel + prevM + Mg_d; // + MFi  add this when you fix the cross product thing
 
 		Matrix<Vector3f, 2, 1> nets(F_net, M_net);
 
 		return nets;
-		
+
 
 	}
 
@@ -167,13 +186,13 @@ public:
 			Vector2d currFM = get_segmentfm(p_tend, f_tend, i, disk_num, masses, prev_MF, arclength, th);
 			Matrix<float, 3, 3> G_mati = get_transmat(i, 0, arclength, th);
 			Disk di(th, arclength, currFM[0], currFM[1], G_mati);
-			
-			
+
+
 
 		}
 
 	}
-	
+
 	//sets up parameters for theta solver
 	struct theta_params {
 		double arclength;
@@ -183,7 +202,7 @@ public:
 		int cur_disk;
 		Vector2d weights;
 		Matrix<Vector3f, 2, 1> prev_MF;
-		
+
 	};
 
 	int thetas(const gsl_vector* x, void* p, gsl_vector* f) {
@@ -201,7 +220,7 @@ public:
 		const double theta0 = gsl_vector_get(x, 0);
 
 		const double y0 = theta0 / arclength;
-		const double y1 = ((get_segmentfm(p_ten, f_tend, i-1, i, weights, prev_MF, arclength, theta0))[1]).norm();
+		const double y1 = ((get_segmentfm(p_ten, f_tend, i - 1, i, weights, prev_MF, arclength, theta0))[1]).norm();
 
 		gsl_vector_set(f, 0, y0);
 		gsl_vector_set(f, 1, y1); //May to actually use "magnitude" thing
@@ -226,17 +245,16 @@ public:
 		//^may affect runtime to append?
 		//could expand to n = number of disks, make a n_disk dimension theta vector, must change get_FM to accomodate 
 		const size_t n = 1;
-		struct theta_params p = { arclength,  E, inertia, p_ten, f_tend, cur_disk, weights, prev_MF};
-		
+		struct theta_params p = { arclength,  E, inertia, p_ten, f_tend, cur_disk, weights, prev_MF };
+
 		//Attempted to implement type change solution. I am doing something wrong
-		//I swear to Allah that I will figure GSL out or so help me 
 		Robot* ptr2 = this;
 		auto ptr = [=](const gsl_vector* x, void* p, gsl_vector* f)->int {return ptr2->thetas(x, p, f); };
 		gsl_function_pp<decltype(ptr)> Fp(ptr);
-		gsl_multiroot_function* f = static_cast<gsl_multiroot_function*>(&Fp);
-		
-		
-		double x_init =  -10.0;
+		gsl_multiroot_function* F = static_cast<gsl_multiroot_function*>(&Fp);
+
+
+		double x_init = -10.0;
 		gsl_vector* x = gsl_vector_alloc(n);
 
 		gsl_vector_set(x, 0, x_init);
@@ -245,11 +263,11 @@ public:
 		s = gsl_multiroot_fsolver_alloc(T, 1);
 		//In the example they have a dereferenece operator before f, however f is already a pointer and i got rid of it
 		//It works apparently?
-		gsl_multiroot_fsolver_set(s, f, x);
+		gsl_multiroot_fsolver_set(s, F, x);
 
 
 		//below is ripped from the GNU website so i can see the thing happen
-		print_state (iter, s);
+		print_state(iter, s);
 
 		do {
 			iter++;
@@ -269,7 +287,7 @@ public:
 		gsl_multiroot_fsolver_free(s);
 		gsl_vector_free(x);
 		return 0;
-	
+
 	}
 
 	//below is also ripped from the GNU to see solver do its thing
@@ -283,36 +301,22 @@ public:
 			gsl_vector_get(s->f, 0),
 			gsl_vector_get(s->f, 1));
 	}
-	
+
 
 };
 
-//template?
-template< typename F >
-class gsl_function_pp : public gsl_multiroot_function {
-public:
-	gsl_function_pp(const F& func) : _func(func) {
-		function = &gsl_function_pp::invoke;
-		params = this;
-	}
-private:
-	const F& _func;
-	static double invoke(double x, void* params) {
-		return static_cast<gsl_function_pp*>(params)->_func(x);
-	}
-};
+
 
 int main() {
-	
 	//Initialize values
 	int ft1 = 2, ft2 = 2, num_d = 1, roh = 6450; // tendon forces and disk number
 	float radius_d = 0.5, g = 9.81, E = 54 * 10 ^ 9, l = 0.2;  // radius of disk
 	float d_mass = 0.005, t_mass = 0.0005; // mass of each disk and tube segment
 
-	Matrix<Vector3f, 2, 1> prev(0,0);
+	Matrix<Vector3f, 2, 1> prev(0, 0);
 	//Organizing Matrices
 	Matrix<float, 2, 1> r1, r2, p_tend, f_tend;
-	Vector2d weights(d_mass*9.81, t_mass*9.81); // matrix intialization
+	Vector2d weights(d_mass * 9.81, t_mass * 9.81); // matrix intialization
 	r1 << radius_d, 0;
 	r2 << -radius_d, 0;
 	p_tend << r1, r2; // Creates matrix housing positions of tendons in x-y coordinates
@@ -322,14 +326,14 @@ int main() {
 	Robot x(l, radius_d, 9.81, num_d, E, weights, f_tend);
 
 	auto y = x.get_theta(2, p_tend, 1, prev);
-	
-
-	
 
 
-	
-	
-	
+
+
+
+
+
+
 
 
 
@@ -339,7 +343,7 @@ int main() {
 	//timing functions
 	auto t1 = std::chrono::high_resolution_clock::now();
 	//Place function i want to test here:
-	
+
 
 	auto t2 = std::chrono::high_resolution_clock::now();
 
@@ -347,8 +351,5 @@ int main() {
 
 	std::cout << duration;
 	return 0;
-	
+
 }
-
-
-
